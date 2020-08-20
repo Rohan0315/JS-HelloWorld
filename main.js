882282eab0e6e374a7b4d594075d7dcbed761891
Cho min woo <shsn04115@naver.com>
2020-08-20 12:29:11 +0900

    eeeeeeeeeeeeeeeeeeeeeeeee

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-console.log("집에 갈려면 어떻게 해야할까요?")
+console.log("제발 집으로 보내줘요")
+console.log("발로해도 알아듣겠다")
+console.log("집에 왜 안보내주는거에요?")
+console.log("애들이 다 죽어가요")
+console.log("보내주지도 않을꺼면 왜 수업을 하는거에요?")
+console.log("내가 하교시간을 얼마나 기다렸는데")
+console.log("주님곁으로 갈꺼같아요")
+console.log("세금 걷으면서 일안해요?")
+console.log("요오 집이다!!!")
 
